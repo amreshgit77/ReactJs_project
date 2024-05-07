7ec837b9eb9d9565909e3fada105be2ec0115227
Amresh Kumar <amreshnew77@gmail.com>
2024-05-07 18:46:03 +0530

    Updates

---
+++ precache-manifest.11ba354aaee71e80f290fbc4c366b262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404183d822cb8c115aaf3f8820008fdd",
+    "url": "/ReactJs_project/index.html"
+  },
+  {
+    "revision": "883f282aaadc150d897f",
+    "url": "/ReactJs_project/static/css/main.dde9f92f.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/ReactJs_project/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/ReactJs_project/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "883f282aaadc150d897f",
+    "url": "/ReactJs_project/static/js/main.9866f55d.chunk.js"
+  },
+  {
+    "revision": "b2509d2c173f9bbb1459",
+    "url": "/ReactJs_project/static/js/runtime~main.d82582fb.js"
+  }
+]);
